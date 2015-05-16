948ad837d325e327f76b1d1229f539fb90ebc5c6
Niek Weevers <niekweevers@gmail.com>
2015-05-16 14:26:44 +0200

    Enkele aanpassingen

--- wp-content/themes/basis/footer.php
+++ wp-content/themes/basis/footer.php
@@ -2,6 +2,8 @@
 
         <?php wp_footer(); ?>
 
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
         <script>
             head.js(
                 { all: 'resources/scripts/all.min.js' }
--- wp-content/themes/basis/header.php
+++ wp-content/themes/basis/header.php
@@ -1,42 +1,43 @@
 <!DOCTYPE html>
 <html class="no-js" lang="nl">
-    <head>
-        <meta charset="utf-8">
-        <meta name="apple-mobile-web-app-capable" content="yes">
+	<head>
+		<meta charset="utf-8">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
 
-        <title><?php wp_title( '-', true, 'right' ); ?></title>
+		<title><?php wp_title( '-', true, 'right' ); ?></title>
 
-        <?php wp_head(); ?>
+		<?php wp_head(); ?>
 
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-        <meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/browserconfig.xml" />
+		<meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/browserconfig.xml" />
 
-        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-        <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon.png">
+		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon.png">
 
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/resources/css/all.css">
-        <script src="<?php echo get_template_directory_uri(); ?>/resources/scripts/libs/header.min.js"></script>
-    </head>
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/resources/css/all.css">
+		<script src="<?php echo get_template_directory_uri(); ?>/resources/scripts/libs/header.min.js"></script>
+	</head>
 
-    <body>
-        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="Homepage <?php echo get_bloginfo( 'name' ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/resources/img/logo.png" alt="Logo <?php echo get_bloginfo( 'name' ); ?>"></a>
-                    
-        <nav class="mainnav">
-            <?php
-                wp_nav_menu(
-                    array(
-                        'theme_location' => 'primary',
-                        'container' => false,
-                        'menu_class' => 'level0',
-                        'items_wrap' => '<ul class="%2$s">%3$s</ul>'
-                    )
-                );
-            ?>
-        </nav>
+	<body>
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="Homepage <?php echo get_bloginfo( 'name' ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/resources/img/logo.png" alt="Logo <?php echo get_bloginfo( 'name' ); ?>"></a>
+					
+		<nav class="mainnav">
+			<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'primary',
+						'container' => false,
+						'menu_class' => 'level0',
+						'items_wrap' => '<ul class="%2$s">%3$s</ul>'
+					)
+				);
+			?>
+		</nav>
 
-        <?php get_search_form(); ?>
+		<?php get_search_form(); ?>
 
-        <?php if ( function_exists('yoast_breadcrumb') ) {
-            yoast_breadcrumb('<div class="breadcrumbs">','</div>');
-        } ?>
+		<?php if ( function_exists('yoast_breadcrumb') ) {
+			yoast_breadcrumb('<div class="breadcrumbs">','</div>');
+		} ?>
