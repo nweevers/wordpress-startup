693a69ac641b7ca4dc0688b75a6c497c128b1f90
Niek Weevers <niekweevers@gmail.com>
2016-03-21 20:47:43 +0100

    Config puntjes

--- wp-config-test.php
+++ wp-config-test.php
@@ -9,7 +9,7 @@ define('DB_HOST', 'localhost');
 /**
  * Waardes uit options table overrulen
  */
-define( 'WP_HOME', 'http://acceptatie.onli.nl/domein.nl');
+define( 'WP_HOME', 'http://acceptatie.onli.nl/domein.nl/www');
 define( 'WP_SITEURL', WP_HOME);
 
 /**
@@ -17,3 +17,7 @@ define( 'WP_SITEURL', WP_HOME);
  */
 define( 'WP_DEBUG', false );
 define( 'WP_CACHE', true );
+
+define('FTP_USER', 'onli'); // Your FTP username
+define('FTP_PASS', 'BBAHcF3d'); // Your FTP password
+define('FTP_HOST', 'ftp.onli.nl:21'); // Your FTP URL:Your FTP port
